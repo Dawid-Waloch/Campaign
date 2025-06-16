3d81e8753fd2d45a28bb50a79cf6b4620df01754
Dawid <dawwal1900@gmail.com>
2025-06-16 21:41:45 +0200

    createRequest function added and onlyManager modifier added

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,9 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
     address public manager;
     uint public minimumContribiution;
     address[] public approvers;
+    Request[] public requests;
     mapping(address => uint) public balances;
 
     constructor(uint minimum) {
@@ -12,16 +20,32 @@ contract Campaign {
         minimumContribiution = minimum;
     }
 
-    function contribiute(uint donation) public minimumDonation(donation) {
+    function contribiute() public payable minimumDonation {
         approvers.push(msg.sender);
+    }
+
+    function createRequest(string memory description, uint value, address recipient) public onlyManager {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+        
+        requests.push(newRequest);
     }
 
     function getApprovers() public view returns (address[] memory) {
         return approvers;
     }
 
-    modifier minimumDonation(uint donation) {
-        require(donation >= minimumContribiution);
+    modifier onlyManager() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    modifier minimumDonation() {
+        require(msg.value >= minimumContribiution);
         _;
     }
 
