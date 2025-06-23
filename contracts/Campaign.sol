90686ec1259c890486c6ec16b1a791b24e6d6065
Dawid <dawwal1900@gmail.com>
2025-06-23 19:15:24 +0200

    More functionallity added

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = address(new Campaign(minimum, msg.sender));
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
 contract Campaign {
     struct Request {
         string description;
@@ -15,31 +28,48 @@ contract Campaign {
     uint public minimumContribiution;
     mapping(address => bool) public approvers;
     Request[] public requests;
+    uint public approversCount;
 
-    constructor(uint minimum) {
-        manager = msg.sender;
+    constructor(uint minimum, address creator) {
+        manager = creator;
         minimumContribiution = minimum;
     }
 
     function contribiute() public payable minimumDonation {
-        approvers.push(msg.sender);
+        require(!approvers[msg.sender], "Already contributed");
+
         approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(string memory description, uint value, address recipient) public onlyManager {
         require(approvers[msg.sender]);
-        Request memory newRequest = Request({
-            description: description,
-            value: value,
-            recipient: recipient,
-            complete: false
-        });
-        
-        requests.push(newRequest);
+        Request storage newRequest = requests.push();
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
     }
 
-    function getApprovers() public view returns (address[] memory) {
-        return approvers;
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public onlyManager {
+        Request storage request = requests[index];
+
+        require(!request.complete);
+        require((approversCount / 2) < request.approvalCount);
+
+        payable(request.recipient).transfer(request.value);
+        request.complete = true;
     }
 
     modifier onlyManager() {
