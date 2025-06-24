2abde0d4ffac7607b34a900ce288eba9b627dff2
Dawid <dawwal1900@gmail.com>
2025-06-24 18:19:20 +0200

    Compile functionallity updated

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,26 +1,40 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const solc = require('solc');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
 
 const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 const source = fs.readFileSync(campaignPath, 'utf-8');
 
 const input = {
-  language: 'Solidity',
-  sources: {
-    'Campaign.sol': {
-      content: source,
+    language: 'Solidity',
+    sources: {
+        'Campaign.sol': {
+            content: source
+        }
     },
-  },
-  settings: {
-    outputSelection: {
-      '*': {
-        '*': ['*'],
-      },
-    },
-  },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['abi', 'evm.bytecode']
+            }
+        }
+    }
 };
 
-console.log(JSON.parse(solc.compile(JSON.stringify(input))).contracts)
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol'].Campaign;
+fs.ensureDirSync(buildPath);
+
+for (let contract in output.contracts['Campaign.sol']) {
+    fs.outputJsonSync(
+        path.resolve(__dirname, 'build', contract.replace(':', '') + '.json'),
+        output.contracts['Campaign.sol'][contract]
+    );
+}
+
+// module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+//     'Campaign.sol'
+// ].Campaign;
