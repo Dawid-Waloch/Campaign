aebb0ef122d8d8fadbd05b28257a7634a6a0a68c
Dawid <dawwal1900@gmail.com>
2025-06-30 22:33:01 +0200

    Changing deployed address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
-const address = '0x75991c94813a2Bd9203c094245f027404f267039';
+const address = '0x167eDB915E732DEA407dC8573dD320409f5Cd603';
 
 const instance = new web3.eth.Contract(CampaignFactory.abi, address);
 
