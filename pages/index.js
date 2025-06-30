0386dd867aeb2abb774d9131acd309496186174a
Dawid <dawwal1900@gmail.com>
2025-06-30 22:34:31 +0200

    Home page refactored to correctly show deployed campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import factory from '../ethereum/factory';
 
-const Home = () => {
-    useEffect(async () => {
-        const campaigns = await factory.methods.deployedCampaigns().call();
-        console.log(campaigns);
-    }, []);
+export const getStaticProps = async () => {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        console.log(campaigns)
 
-    return <h2>Campaigns </h2>;
+        return {
+            props: {
+                campaigns,
+            }
+        };
+    }
+
+const CampaginIndex = ({ campaigns }) => {
+    return <h2>{campaigns[0]}</h2>;
 };
 
-export default Home;
+export default CampaginIndex;
