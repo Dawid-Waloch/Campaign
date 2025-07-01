92cec8efe5f340704b569310870f6970c3990faf
Dawid <dawwal1900@gmail.com>
2025-07-01 21:32:16 +0200

    Semanti UI styling added and deployed campaigns displaying added

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import { Button, Card } from 'semantic-ui-react';
+import Link from 'next/link';
+import 'semantic-ui-css/semantic.min.css';
 
 import factory from '../ethereum/factory';
 
 export const getStaticProps = async () => {
-        const campaigns = await factory.methods.getDeployedCampaigns().call();
-        console.log(campaigns)
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        return {
-            props: {
-                campaigns,
-            }
-        };
-    }
+    return {
+        props: {
+            campaigns,
+        }
+    };
+}
+
+const CampaignCard = ({ address }) => (
+    <Card fluid>
+        <Card.Content>
+            <Card.Header>{address}</Card.Header>
+            <Card.Description>
+                <Link href={`campaigns/${address}`}>View campaign</Link>
+            </Card.Description>
+        </Card.Content>
+    </Card>
+);
 
 const CampaginIndex = ({ campaigns }) => {
-    return <h2>{campaigns[0]}</h2>;
+    return (
+        <div>
+            <h2>Deployed campaigns</h2>
+            <Card.Group>
+                {campaigns.map((address) => (
+                    <CampaignCard key={address} address={address} />
+                ))}
+            </Card.Group>
+        </div>
+    );
 };
 
 export default CampaginIndex;
