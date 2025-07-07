9d4e6ae9bf61d3a6129e559db9c818dd5bebda7b
Dawid <dawwal1900@gmail.com>
2025-07-07 22:04:14 +0200

    Show page deleted and added new campaign page instead

---
+++ pages/campaigns/new.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+import 'semantic-ui-css/semantic.min.css';
+
+const NewCampaign = () => {
+    return (
+        <Layout>
+            <h1>New Campaign!</h1>
+        </Layout>
+    );
+};
+
+export default NewCampaign;
--- pages/show.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Show = () => {
-    return <h1>Coś tam coś tam coś tam</h1>;
-};
-
-export default Show;
