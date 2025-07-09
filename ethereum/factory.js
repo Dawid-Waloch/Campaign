5b6fea0c504ca0b640dc3fdf5613ad25631312c4
Dawid <dawwal1900@gmail.com>
2025-07-09 22:29:40 +0200

    address in factory file changed and campaign file added for the same reason as factory file before

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
-const address = '0x167eDB915E732DEA407dC8573dD320409f5Cd603';
+const address = '0x1db026A1F8BC14c5bC18fC321ac12f39Fe9eee80';
 
 const instance = new web3.eth.Contract(CampaignFactory.abi, address);
 
