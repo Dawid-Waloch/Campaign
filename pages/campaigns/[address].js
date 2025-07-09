6599011a64b32087d9324a7f42286e015f841461
Dawid <dawwal1900@gmail.com>
2025-07-09 22:57:36 +0200

    campaign show page updated, all necessary information collected

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 
-const CampaignShow = () => {
-    const router = useRouter();
-    const { address } = router.query;
+export const getServerSideProps  = async (context) => {
+    const { address } = context.query;
+    const campaign = Campaign(address);
+    const summary = await campaign.methods.getSummary().call();
 
-    const getStaticProps = async () => {
-        const campaign = Campaign(address);
+    return {
+        props: {
+            minimumContribution: summary[0].toString(),
+            balance: summary[1].toString(),
+            requestsCount: summary[2].toString(),
+            approversCount: summary[3].toString(),
+            manager: summary[4].toString(),
+        },
     };
+};
 
+const CampaignShow = ({ 
+    minimumContribution,
+    balance,
+    requestsCount,
+    approversCount,
+    manager
+}) => {
+    
     return (
         <Layout>
             <h2>Campaign show</h2>
-            {address}
         </Layout>
     );
 };
