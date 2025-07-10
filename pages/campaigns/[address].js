e0bcb20034bf8df9d8426a09bfc8d96ab942f633
Dawid <dawwal1900@gmail.com>
2025-07-10 21:58:06 +0200

    Specific details displaying for specific campaign

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react';
 
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
+import CardItem from '../../components/CardItem';
 
 export const getServerSideProps  = async (context) => {
     const { address } = context.query;
@@ -26,10 +29,45 @@ const CampaignShow = ({
     approversCount,
     manager
 }) => {
+
+    const cardsData = [
+        {
+            header: manager,
+            meta: 'Address of Manager',
+            description: 'The manager created this campaign and can create requests to withdraw money'
+        },
+        {
+            header: minimumContribution,
+            meta: 'Minimum Contribution (wei)',
+            description: 'You must contribute at least this much wei to become an approver'
+        },
+        {
+            header: requestsCount,
+            meta: 'Number of Requests',
+            description: 'A request tries to withdraw money from the contract. Requests must be approved by approvers'
+        },
+        {
+            header: approversCount,
+            meta: 'Number of approvers',
+            description: 'Number of people who have already donated to this campaign'
+        },
+        {
+            header: web3.utils.fromWei(balance, 'ether'),
+            meta: 'Campaign Balance (ether)',
+            description: 'The balance is how much money this campaign has left to spend'
+        },
+    ];
     
     return (
         <Layout>
             <h2>Campaign show</h2>
+            
+            <Card.Group>
+                {cardsData.map((card, index) => (
+                    <CardItem key={index} {...card} />
+                ))}
+            </Card.Group>
+
         </Layout>
     );
 };
