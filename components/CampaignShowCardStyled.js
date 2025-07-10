43323c586e5c1bda778a7ad41269c3c7693190e3
Dawid <dawwal1900@gmail.com>
2025-07-10 21:56:43 +0200

    Card with campaign details added and separated file with styles to it

---
+++ components/CampaignShowCardStyled.js
@@ -0,0 +1,6 @@
+import { Card } from "semantic-ui-react";
+import styled from "styled-components";
+
+export const CampaignShowCardStyled = styled(Card)`
+    overflow-wrap: break-word;
+`;
---
+++ components/CardItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { CampaignShowCardStyled } from "./CampaignShowCardStyled";
+import { Card } from "semantic-ui-react";
+
+const CardItem = ({ header, meta, description }) => (
+        <CampaignShowCardStyled>
+            <Card.Content>
+                <Card.Header>{header}</Card.Header>
+                <Card.Meta>{meta}</Card.Meta>
+                <Card.Description>{description}</Card.Description>
+            </Card.Content>
+        </CampaignShowCardStyled>
+);
+
+export default CardItem;
