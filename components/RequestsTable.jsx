a15f19340544a17889e14a6d3b795b6afc3ec480
Dawid <dawwal1900@gmail.com>
2025-07-17 20:13:59 +0200

    Requests displayed to some point without some information in table (will be added in the following commits)

--- components/RequestsTable.jsx
+++ components/RequestsTable.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import { Button, Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from "semantic-ui-react";
+import web3 from "../ethereum/web3";
 
-const RequestsTable = ({requests, approversCount}) => {
+const RequestsTable = ({ requests }) => {
     return (
         <Table>
         <TableHeader>
             <TableRow>
                 <TableHeaderCell>ID</TableHeaderCell>
                 <TableHeaderCell>Description</TableHeaderCell>
+                <TableHeaderCell>Amount</TableHeaderCell>
                 <TableHeaderCell>Recipient</TableHeaderCell>
                 <TableHeaderCell>ApprovalCount</TableHeaderCell>
                 <TableHeaderCell>Approve</TableHeaderCell>
                 <TableHeaderCell>Finalize</TableHeaderCell>
             </TableRow>
-            <TableBody>
-                {requests.map((request, index) => {
-                    <TableRow key={index}>
-                        <TableCell>{index}</TableCell>
-                        <TableCell>{request.description}</TableCell>
-                        <TableCell>{request.amount}</TableCell>
-                        <TableCell>{request.recipient}</TableCell>
-                        <TableCell>{request.approvalCount}/{approversCount/2}</TableCell>
-                        <TableCell>
-                            <Button success>Approve</Button>
-                        </TableCell>
-                        <TableCell>{index}</TableCell>
-                    </TableRow>
-                })};
-                
-            </TableBody>
         </TableHeader>
+        <TableBody>
+            {requests.map((request, index) => (
+                <TableRow key={index}>
+                    <TableCell>{index}</TableCell>
+                    <TableCell>{request.description}</TableCell>
+                    <TableCell>{web3.utils.fromWei(request.value.toString(), "ether")}</TableCell>
+                    <TableCell>{request.recipient}</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>
+                        <Button positive>Approve</Button>
+                    </TableCell>
+                    <TableCell>{index}</TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
     </Table>
     );
 };
--- pages/campaigns/[address]/requests/index.jsx
+++ pages/campaigns/[address]/requests/index.jsx
@@ -3,19 +3,50 @@ import Layout from "../../../../components/Layout/Layout";
 import Link from "next/link";
 import { Button } from "semantic-ui-react";
 
-const getStaticProps = (context) => {
-    const { address } = context.query;
+import RequestsTable from "../../../../components/RequestsTable";
+import web3 from "../../../../ethereum/web3";
+import Campaign from "../../../../ethereum/campaign";
 
-    return { address };
+export const getServerSideProps = async (context) => {
+    const { address } = context.query;
+    const accounts = await web3.eth.getAccounts();
+    const campaign = Campaign(address);
+    const requestsCount = await campaign.methods.getRequestsCount().call();
+
+    const requestsRaw = await Promise.all(
+        Array(parseInt(requestsCount))
+            .fill()
+            .map((_, index) => {
+                return campaign.methods.requests(index).call();
+            })
+    );
+
+    const requests = requestsRaw.map((request) => ({
+        description: request.description,
+        value: request.value.toString(),
+        recipient: request.recipient,
+        complete: request.complete,
+        approvalCount: request.approvalCount.toString()
+    }));
+
+    return {
+        props: {
+            address: address,
+            requests: requests,
+            requestsCount: requestsCount.toString()
+        }
+    };
 };
 
-const RequestIndex = ({ address }) => {
+const RequestIndex = ({ address, requests, requestsCount }) => {
     return (
         <Layout>
             <h3>Requests</h3>
             <Link href={`/campaigns/${address}/requests/new`}>
                 <Button primary>Add Request</Button>
             </Link>
+            
+            <RequestsTable requests={requests} />
         </Layout>
     )
 };
